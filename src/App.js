ab6ed68f121c452add0695cbf8fc21b7b8d51c54
Jalo Moster <jlvmoster@gmail.com>
2020-09-15 19:20:35 -0400

    Minor enhancements/bug fixes
    - fix bug where login does not auto populate patient chip

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useSnackbar } from 'notistack';
+import { API, Auth, graphqlOperation } from 'aws-amplify';
 import Box from '@material-ui/core/Box';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import AssignmentIcon from '@material-ui/icons/Assignment';
@@ -19,6 +21,9 @@ import ProfileScreen from './screens/ProfileScreen';
 import TheseusScreen from './screens/TheseusScreen';
 import hocFactory from './util/hocFactory';
 
+import { getPerson, getSession } from './graphql/queries';
+import { SET_PATIENT, SET_SESSION, SET_USER } from './contexts/Session/actions';
+
 const menu = [
   { label: 'Profile', path: '/profile', icon: <AccountCircleIcon />, screen: <ProfileScreen /> },
   { label: 'Theseus', path: '/theseus', icon: <AssignmentIcon />, screen: <TheseusScreen /> },
@@ -28,12 +33,60 @@ const menu = [
 const HOME = '/theseus';
 
 const App = () => {
-  const { state } = useSession();
-  const { patient } = state;
+  const { enqueueSnackbar } = useSnackbar();
+  const { state, dispatch } = useSession();
+  const { user } = state;
+
+  React.useEffect(() => {
+    (async () => {
+      const user = await Auth.currentAuthenticatedUser().catch(error => {
+        enqueueSnackbar(`Whoops! Something went wrong when fetching current user: ${error.message}`, {
+          variant: 'error',
+        });
+      });
+
+      dispatch({ type: SET_USER, payload: user });
+    })();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  React.useEffect(() => {
+    let mounted = true;
+    (async () => {
+      let result1;
+      let result2;
+      if (user) {
+        result1 = await API.graphql(graphqlOperation(getSession, { session_id: user.username })).catch(error => {
+          enqueueSnackbar(`Whoops! Something went wrong when fetching a session: ${error.message}`, {
+            variant: 'error',
+          });
+        });
+
+        result2 = await API.graphql(
+          graphqlOperation(getPerson, { person_id: result1.data.getSession.patient_id })
+        ).catch(error => {
+          enqueueSnackbar(`Whoops! Something went wrong when fetching a patient by session: ${error.message}`, {
+            variant: 'error',
+          });
+        });
+
+        if (mounted) {
+          dispatch({ type: SET_SESSION, payload: result1.data.getSession });
+          dispatch({ type: SET_PATIENT, payload: result2.data.getPerson });
+        } else {
+          API.cancel(result1, 'App unmounted');
+          API.cancel(result2, 'App unmounted');
+        }
+      }
+    })();
+
+    return () => {
+      mounted = false;
+    };
+  }, [user]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Box>
-      <TopBar patient={patient} />
+      <TopBar />
       <Box pb={7}>
         <RootNavigation menu={menu} homePath={HOME} />
       </Box>
--- src/hocs/withAuth.js
+++ src/hocs/withAuth.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import { useSnackbar } from 'notistack';
-import { API, graphqlOperation } from 'aws-amplify';
 import { AmplifyAuthenticator, AmplifyContainer, AmplifySignIn } from '@aws-amplify/ui-react';
 import { Auth, appendToCognitoUserAgent } from '@aws-amplify/auth';
 import { onAuthUIStateChange, AuthState } from '@aws-amplify/ui-components';
 
-import { getPerson, getSession } from '../graphql/queries';
-import { SET_PATIENT, SET_SESSION, SET_USER } from '../contexts/Session/actions';
-import useSession from '../hooks/useSession';
-
 export default Component => props => {
   const [signedIn, setSignedIn] = React.useState(false);
-  const { enqueueSnackbar } = useSnackbar();
-  const { state, dispatch } = useSession();
-  const { user } = state;
 
   const checkUser = () => {
     setUser();
@@ -30,7 +21,6 @@ export default Component => props => {
   const setUser = async () => {
     try {
       const user = await Auth.currentAuthenticatedUser();
-      dispatch({ type: SET_USER, payload: user });
       if (user) setSignedIn(true);
     } catch (err) {
       console.error(err);
@@ -43,41 +33,6 @@ export default Component => props => {
     // checkUser returns an "unsubscribe" function to stop side-effects
     return checkUser();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-  React.useEffect(() => {
-    let mounted = true;
-    (async () => {
-      if (user) {
-        let result1;
-        let result2;
-        result1 = await API.graphql(graphqlOperation(getSession, { session_id: user.username })).catch(error => {
-          enqueueSnackbar(`Whoops! Something went wrong when fetching a session: ${error.message}`, {
-            variant: 'error',
-          });
-        });
-
-        result2 = await API.graphql(
-          graphqlOperation(getPerson, { person_id: result1.data.getSession.patient_id })
-        ).catch(error => {
-          enqueueSnackbar(`Whoops! Something went wrong when fetching a patient by session: ${error.message}`, {
-            variant: 'error',
-          });
-        });
-
-        if (mounted) {
-          dispatch({ type: SET_SESSION, payload: result1.data.getSession });
-          dispatch({ type: SET_PATIENT, payload: result2.data.getPerson });
-        } else {
-          API.cancel(result1, 'withAuth unmounted');
-          API.cancel(result2, 'withAuth unmounted');
-        }
-      }
-    })();
-
-    return () => {
-      mounted = false;
-    };
-  }, [dispatch, enqueueSnackbar, user]);
 
   if (!signedIn) {
     return (
