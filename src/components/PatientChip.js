793447931af0193bfb1272481ae7c2002521f395
Jalo Moster <jlvmoster@gmail.com>
2020-09-15 00:46:27 -0400

    Add ability to GET profile picture from public directory on s3

--- src/components/PatientChip.js
+++ src/components/PatientChip.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import { Storage } from 'aws-amplify';
 import Avatar from '@material-ui/core/Avatar';
 import Box from '@material-ui/core/Box';
 import Chip from '@material-ui/core/Chip';
 import FaceIcon from '@material-ui/icons/Face';
 
+import { SHOW_SNACKBAR } from '../contexts/Snackbar/actions';
+import useSnackbar from '../hooks/useSnackbar';
+
 export default ({ patient }) => {
-  const getInitials = () => {
-    const first = patient?.name.first.charAt(0);
-    const last = patient?.name.last.charAt(0);
-    return first + last;
-  };
+  const [picture, setPicture] = React.useState('');
+  const { dispatch } = useSnackbar();
+
+  React.useEffect(() => {
+    (async () => {
+      if (patient) {
+        const response = await Storage.get('patients/' + patient.person_id + '.jpg').catch(error => {
+          dispatch({
+            type: SHOW_SNACKBAR,
+            payload: {
+              message: `Whoops! Something went wrong when retrieving public object from s3: ${error.message}`,
+              anchor: { vertical: 'bottom' },
+              direction: 'up',
+            },
+          });
+        });
+
+        setPicture(response);
+      }
+    })();
+  }, [patient]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Box>
@@ -18,7 +38,7 @@ export default ({ patient }) => {
           color='primary'
           label={`${patient?.name.last}, ${patient?.name.first}`}
           variant='outlined'
-          avatar={<Avatar>{getInitials()}</Avatar>}
+          avatar={<Avatar src={picture} />}
           clickable
         />
       ) : (
