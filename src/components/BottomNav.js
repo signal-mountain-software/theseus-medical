8ad48376a5cbc05d82b9c7318effe57cb3809c1f
Jalo Moster <jlvmoster@gmail.com>
2020-09-18 18:43:57 -0400

    Rename a function

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -7,13 +7,13 @@ import Box from '@material-ui/core/Box';
 export default ({ menu, homePath }) => {
   const [value, setValue] = React.useState(homePath);
 
-  const handleChange = (event, newValue) => {
+  const onChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
     <Box width='100%' position='fixed' top='auto' bottom={0} zIndex={3} clone>
-      <BottomNavigation value={value} onChange={handleChange}>
+      <BottomNavigation value={value} onChange={onChange}>
         {menu.map(item => (
           <BottomNavigationAction
             key={item.path}
