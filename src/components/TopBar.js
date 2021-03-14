723bdaadcc09c2fc547a3b382b4b891b99a6f4a7
Jalo Moster <jlvmoster@gmail.com>
2021-03-14 17:15:53 -0400

    Fix install still showing on non-iOS standalone apps

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -46,7 +46,6 @@ const ITEM_HEIGHT = 48;
 const nav = getNavigatorInstance();
 
 export default () => {
-  const [showInstall, setShowInstall] = React.useState(!(nav && nav.standalone) || !prompt);
   const [showIOSDialog, setShowIOSDialog] = React.useState(false);
   const [hide, setHide] = React.useState(true);
   const [open, setOpen] = React.useState(false);
@@ -56,6 +55,7 @@ export default () => {
   const { patient, roles, session } = state;
 
   const [prompt, setPrompt] = useRecoilState(promptState);
+  const [showInstall, setShowInstall] = React.useState(!(nav && nav.standalone) || !prompt);
 
   const handleClick = event => {
     setAnchorEl(event.currentTarget);
