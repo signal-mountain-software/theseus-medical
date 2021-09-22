88472e5de5cd6d170307aa14854a73cde8e1d2b9
rsteelefamily <rsteele@signalmountainsoftware.com>
2021-09-22 09:46:40 -0400

    avoid cached version when accessing from href

--- src/components/sections/ActivitySection.js
+++ src/components/sections/ActivitySection.js
@@ -725,7 +725,7 @@ export default ({ patient, session, newFact, setNewFact }) => {
                     <Box display='flex' flexDirection='row' justifyContent='flex-start' alignItems='center'>
                       <Box display='flex' flexDirection='column' width='95%' textOverflow='ellipsis'>
                         {activity.type === 'document' ? 
-                          <a href={activity.default_value} style={{color: 'inherit', textDecoration: 'none'}} target="_blank" rel="noopener noreferrer">
+                          <a href={activity.default_value + '?a=' + new Date().getTime()} style={{color: 'inherit', textDecoration: 'none'}} target="_blank" rel="noopener noreferrer">
                             <Typography variant='h5'>{activity.name}</Typography>
                           </a> 
                           :
