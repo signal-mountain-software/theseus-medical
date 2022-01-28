6ac6e7922b92d877257343e5f98664def9ea170d
rsteelefamily <rsteele@signalmountainsoftware.com>
2022-01-28 09:36:49 -0500

    Release number

--- src/components/sections/ActivitySection.js
+++ src/components/sections/ActivitySection.js
@@ -236,7 +236,7 @@ export default ({ patient, session }) => {
       patient_id: patient.person_id,
       activity_key: '***ERROR_CAUGHT***',
       value: parmMessage,
-      status: `Version = v22.1.24~${errorTime}`,
+      status: `Version = v22.1.28~${errorTime}`,
       session: {
         user_id: patient.person_id,
         session_id: session.client_id,
