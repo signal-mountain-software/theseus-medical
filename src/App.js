7da7f898932c64a9a793079a372ed4eca08dc60d
rsteelefamily <rsteele@signalmountainsoftware.com>
2022-05-13 14:11:55 -0400

    release update

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ const handleWriteError = async (parmMessage) => {
     patient_id: user?.username || 'no info',
     activity_key: '***ERROR_CAUGHT***',
     value: `error.${parmMessage}`,
-    status: `Version = v22.5.11~${errorTime}`,
+    status: `Version = 22.5.13~${errorTime}`,
     session: {
       user_id: user?.username || 'no user logged',
       session_id: 'no session recorded',
