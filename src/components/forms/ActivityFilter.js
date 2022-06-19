dbc68c8caa63b2ab1f0cf6e52cbbbe85b43f7f03
rsteelefamily <rsteele@signalmountainsoftware.com>
2022-06-19 07:42:16 -0400

    correct bug - failing on empty activity list

--- src/components/forms/ActivityFilter.js
+++ src/components/forms/ActivityFilter.js
@@ -163,7 +163,7 @@ export default ({ activityList, activityObject, onCancel, onSelect }) => {
           autoComplete='off'
         />
         <List component='nav'>
-          {activityList.length > 0 && activityList.map((listEntry, x) => (
+          {activityList && activityList.length > 0 && activityList.map((listEntry, x) => (
             (
               listEntry.toLowerCase().includes(activity_filter.toLowerCase()) ?
                 <ListItem
