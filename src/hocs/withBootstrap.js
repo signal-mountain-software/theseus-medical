979e195fa17b02a60f261d3de02b11a570df3ac3
rsteelefamily <rsteele@signalmountainsoftware.com>
2022-12-12 10:29:06 -0500

    release number

--- src/hocs/withBootstrap.js
+++ src/hocs/withBootstrap.js
@@ -278,7 +278,7 @@ export default Component => props => {
               />
             </Card>
             <Typography align='center'>
-              {`AVA version 22.12.04${window.location.href.split('//')[1].slice(0, 1).toUpperCase()}`}
+              {`AVA version 22.12.12${window.location.href.split('//')[1].slice(0, 1).toUpperCase()}`}
             </Typography>
             <CircularProgress />
           </Box>
@@ -583,7 +583,7 @@ export default Component => props => {
   async function updateSession(pSessionID, pSession, pPatient, pProfile, pLogin, pURL, pMessage, pSessionInfo) {
     let attributeValues = {
       ':s': {
-        'version': `v22.12.04`,
+        'version': `v22.12.12`,
         'environment': window.location.href.split('//')[1].charAt(0).toUpperCase(),
         'time': new Date().toString(),
         'signin_status': pMessage,
