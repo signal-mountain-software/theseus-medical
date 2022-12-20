237d65bd8fab43fed043c6395153ed2a1e2d9f77
rsteelefamily <rsteele@signalmountainsoftware.com>
2022-12-20 11:11:29 -0500

    clean-up react warning

--- src/components/sections/AVAMenu.js
+++ src/components/sections/AVAMenu.js
@@ -27,8 +27,6 @@ import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuList from '@material-ui/core/MenuList';
 import MenuItem from '@material-ui/core/MenuItem';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
 
 import Collapse from '@material-ui/core/Collapse';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
