e5f5ff36810c8aa4c50e45d4b8e8fe77b38fcebd
rsteelefamily <rsteele@signalmountainsoftware.com>
2022-12-24 13:00:20 -0500

    release number

--- src/hocs/withBootstrap.js
+++ src/hocs/withBootstrap.js
@@ -275,7 +275,7 @@ export default Component => props => {
               />
             </Card>
             <Typography align='center'>
-              {`AVA version 22.12.21${window.location.href.split('//')[1].slice(0, 1).toUpperCase()}`}
+              {`AVA version 22.12.25${window.location.href.split('//')[1].slice(0, 1).toUpperCase()}`}
             </Typography>
             <CircularProgress />
           </Box>
@@ -585,7 +585,7 @@ export default Component => props => {
   async function updateSession(pSessionID, pSession, pPatient, pProfile, pLogin, pURL, pMessage, pSessionInfo) {
     let attributeValues = {
       ':s': {
-        'version': `v22.12.21`,
+        'version': `v22.12.25`,
         'environment': window.location.href.split('//')[1].charAt(0).toUpperCase(),
         'time': new Date().toString(),
         'signin_status': pMessage,
--- src/screens/Reloader.js
+++ src/screens/Reloader.js
@@ -23,7 +23,7 @@ export default () => {
     return (
         <Box mt={3}>
             <Typography align='center'>
-                {`Loading AVA version 22.12.21${window.location.href.split('//')[1].slice(0, 1).toUpperCase()}`}
+                {`Loading AVA version 22.12.25${window.location.href.split('//')[1].slice(0, 1).toUpperCase()}`}
             </Typography>
         </Box>
     );
