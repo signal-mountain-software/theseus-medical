dee835fa900a91579f2158c6fcd1349530c551fb
rsteelefamily <rsteele@signalmountainsoftware.com>
2023-04-05 00:25:55 -0400

    inline color management

--- src/util/AVAUtilities.js
+++ src/util/AVAUtilities.js
@@ -45,13 +45,13 @@ export function stringToColor(string) {
   for (i = 0; i < string.length; i += 1) {
     hash = string.charCodeAt(i) + ((hash << 5) - hash);
   }
-  let color = '#';
+  let color = '#e';
   for (i = 0; i < 3; i += 1) {
     const value = (hash >> (i * 8)) & 0xff;
     color += `00${value.toString(16)}`.substr(-2);
   }
   /* eslint-enable no-bitwise */
-  return color;
+  return color.slice(0, 7);
 }
 
 export function cl() {
