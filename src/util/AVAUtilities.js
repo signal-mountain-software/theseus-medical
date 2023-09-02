bd1d7de01b348a34e21a200a18cd35e3ee42a27d
rsteelefamily <rsteele@signalmountainsoftware.com>
2023-09-02 01:27:00 -0400

    make sentencecase handle Mc as in McDonald

--- src/util/AVAUtilities.js
+++ src/util/AVAUtilities.js
@@ -172,6 +172,13 @@ export function clt() {
 export function sentenceCase(pString) {
   if (!pString) { return ''; }
   if (typeof (pString) === 'object') { return JSON.stringify(pString); }
+  if (pString.slice(0, 2).toLowerCase() === 'mc') {
+    return (
+      pString.slice(0, 1).toUpperCase() +
+      pString.slice(1, 2).toLowerCase() +
+      pString.slice(2, 3).toUpperCase() +
+      pString.slice(3).toLowerCase()
+  ); }
   return (pString.slice(0, 1).toUpperCase() + pString.slice(1).toLowerCase());
 }
 
