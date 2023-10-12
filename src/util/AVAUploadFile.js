623eff2dcace8d62a40f6a875a6aa22048b789f6
rsteelefamily <rsteele@signalmountainsoftware.com>
2023-10-12 09:45:05 -0400

    improve styling

--- src/util/AVAUploadFile.js
+++ src/util/AVAUploadFile.js
@@ -4,22 +4,24 @@ import { s3, makeArray } from '../util/AVAUtilities';
 
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
-import DialogContentText from '@material-ui/core/DialogContentText';
+import Typography from '@material-ui/core/Typography';
 
 import TextField from '@material-ui/core/TextField';
 
 import CloseIcon from '@material-ui/icons/HighlightOff';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 
-import { AVAclasses } from '../util/AVAStyles';
+import { AVAclasses, AVATextStyle, AVADefaults } from '../util/AVAStyles';
 
 export default ({ onCancel, onLoad, options = {} }) => {
 
   const AVAClass = AVAclasses();
+  let user_fontSize = AVADefaults({ fontSize: 'get' });
 
   // if options.buttonText, use buttontext as follows:
   //  [0] - use as default (not shown when no file has been selected yet)
@@ -52,43 +54,74 @@ export default ({ onCancel, onLoad, options = {} }) => {
   return (
     <Dialog open={forceRedisplay || true} fullWidth
       style={{ marginX: 2, marginTop: 3 }} >
-      <DialogContentText className={AVAClass.AVATitle} id='scroll-dialog-title'>
+      <Typography
+        style={AVATextStyle({
+          size: 1.3,
+          bold: true
+        })}
+        className={AVAClass.AVATitle} id='scroll-dialog-title'>
         {options.title || 'Upload a file'}
-      </DialogContentText>
+      </Typography>
       <Paper component={Box} style={{ maxWidth: 1000 }} overflow='auto' square>
-        {(reactData.uploadList.length > 0) &&
-          <Paper component={Box} style={{ paddingTop: '16px' }} overflow='auto' square>
-            {reactData.uploadList.map((fObj, cIndex) => (
-              <Box display='flex'
-                style={{ marginBottom: '2em', marginLeft: '1em', }}
-                flexDirection='row' key={`ambiguous-${cIndex}`} justifyContent='flex-start' alignItems='center'
-                paddingX={2}
-                paddingY={1}
-                marginRight={2}
-                minWidth={'90%'}
-                border={1}
-                borderRadius={'16px'}
-              >
-                <TextField
-                  className={AVAClass.AVASmallText}
-                  id={`fNameID`}
-                  key={`fNameKey`}
-                  multiline
-                  value={fObj.fName}
-                  helperText={fObj.fType}
-                  onChange={(event) => {
-                    handleChangeTextInput(event, cIndex);
-                  }}
-                  autoComplete='off'
-                />
-              </Box>
-            )
-            )}
-          </Paper>
-        }
-        <DialogActions className={AVAClass.AVABox} style={{ justifyContent: 'center' }}>
-          <Box display='flex' flexDirection='column'>
-            <Box display='flex' flexDirection='row' paddingBottom={1} justifyContent='center' alignItems='center'>
+        <Box
+          display='flex'
+          grow={1}
+          pt={1}
+          id={`contentsColumn`}
+          key={`contentsColumn`}
+          flexDirection='column'
+          justifyContent='center'
+          alignItems='flex-start'
+        >
+          {(reactData.uploadList.length > 0) &&
+            <Paper component={Box} style={{ paddingTop: '16px', paddingBottom: '16px', width: '100%' }} overflow='auto' square>
+              {reactData.uploadList.map((fObj, cIndex) => (
+                <Box display='flex'
+                  flexDirection='column'
+                  my={1}
+                  paddingLeft={2}
+                  paddingRight={2}
+                  mx={1}
+                  justifyContent={'center'}
+                  minHeight={`${user_fontSize * 2}rem`}
+                  border={1}
+                  borderRadius={'16px'}
+                  key={`ambiguous-${cIndex}`}
+                >
+                  <Box display='flex' flexDirection='row' paddingBottom={1} justifyContent='space-between' alignItems='center'>
+                    <Box width='90%' flexGrow={1} >
+                      <TextField
+                        style={{ width: '95%' }}
+                        id={`fNameID`}
+                        key={`fNameKey`}
+                        multiline
+                        inputProps={{ style: { fontSize: `${user_fontSize}rem`, lineHeight: `${user_fontSize * 1.2}rem` } }}
+                        FormHelperTextProps={{ style: { fontSize: `${user_fontSize * 0.75}rem`, lineHeight: `${user_fontSize * 0.9}rem` } }}
+                        value={fObj.fName}
+                        helperText={fObj.fType}
+                        onChange={(event) => {
+                          handleChangeTextInput(event, cIndex);
+                        }}
+                        autoComplete='off'
+                      />
+                    </Box>
+                    <DeleteIcon
+                      edge={'end'}
+                      onClick={() => {
+                        reactData.uploadList.splice(cIndex, 1);
+                        setReactData(reactData);
+                        setForceRedisplay(!forceRedisplay);
+                      }}
+                    />
+                  </Box>
+                </Box>
+              ))}
+            </Paper>
+          }
+          </Box>
+          <DialogActions className={AVAClass.AVABox} style={{ justifyContent: 'center' }}>
+            <Box display='flex' flexDirection='column'>
+              <Box display='flex' flexDirection='row' marginTop={2} paddingBottom={1} justifyContent='center' alignItems='center'>
                 <Button
                   className={AVAClass.AVAButton}
                   style={{ backgroundColor: 'blue', color: 'white' }}
@@ -133,33 +166,33 @@ export default ({ onCancel, onLoad, options = {} }) => {
                     setForceRedisplay(!forceRedisplay);
                   }}
                 />
-              <Button
-                className={AVAClass.AVAButton}
-                style={{ backgroundColor: 'red', color: 'white' }}
-                size='small'
-                variant='outlined'
-                onClick={() => onCancel()}
-                startIcon={<CloseIcon size="small" />}
-              >
-                Exit
-              </Button>
-              {(reactData.uploadList.length > 0) &&
                 <Button
                   className={AVAClass.AVAButton}
-                  style={{ backgroundColor: 'green', color: 'white' }}
+                  style={{ backgroundColor: 'red', color: 'white' }}
                   size='small'
                   variant='outlined'
-                  onClick={() => {
-                    handleSave();
-                  }}
+                  onClick={() => onCancel()}
                   startIcon={<CloseIcon size="small" />}
                 >
-                  {reactData.buttonText}
+                  Exit
                 </Button>
-              }
+                {(reactData.uploadList.length > 0) &&
+                  <Button
+                    className={AVAClass.AVAButton}
+                    style={{ backgroundColor: 'green', color: 'white' }}
+                    size='small'
+                    variant='outlined'
+                    onClick={() => {
+                      handleSave();
+                    }}
+                    startIcon={<CloseIcon size="small" />}
+                  >
+                    {reactData.buttonText}
+                  </Button>
+                }
+              </Box>
             </Box>
-          </Box>
-        </DialogActions>
+          </DialogActions>
       </Paper >
     </Dialog >
   );
