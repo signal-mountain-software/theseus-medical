31873a26c4bbfc00618cb261976cea0c12d0a756
rsteelefamily <rsteele@signalmountainsoftware.com>
2024-03-25 08:24:13 -0400

    remove outdated multiobservation and multiobservationB

--- src/components/forms/DynamicForm.js
+++ src/components/forms/DynamicForm.js
@@ -8,8 +8,7 @@ import NewCalendarEvent from '../dialogs/NewCalendarEvent';
 import MessageForm from '../forms/MessageForm';
 import FileUpload from '../forms/FileUpload';
 import ObservationForm from '../forms/ObservationForm';
-import MultiObservationForm from '../forms/MultiObservationForm';
-import MultiObservationFormB from '../forms/MultiObservationFormB';
+
 import MultiObservationFormC from '../forms/MultiObservationFormC';
 import CheckInCheckOut from '../forms/CheckInCheckOut';
 import RequestDashboard from '../dialogs/RequestDashboard';
@@ -362,33 +361,7 @@ export default ({
         />
       );
     case 'multi_observation':
-      return (
-        <MultiObservationForm
-          fact={newFact}
-          factName={factName}
-          defaultValue={defaultValue}
-          prompt={message}
-          pClient={session.client_id}
-          qualifiers={qualifierTable}
-          listValues={values}
-          onSave={onSave}
-          onClose={onClose}
-        />
-      );
     case 'multi_observation_type2':
-      return (
-        <MultiObservationFormB
-          fact={newFact}
-          factName={factName}
-          defaultValue={defaultObject}
-          prompt={message}
-          pClient={session.client_id}
-          qualifiers={qualifierTable}
-          listValues={values}
-          onSave={onSave}
-          onClose={onClose}
-        />
-      );
     case 'multi_observation_type3':
       return (
         <MultiObservationFormC
